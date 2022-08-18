b28c97b467686261e55201884099a1f43fbba14d
illbison <105686563+illbison@users.noreply.github.com>
2022-08-18 04:20:16 +0300

    Added charset_normalizer to avoid weird encodings

--- regex-filter.py
+++ regex-filter.py
@@ -4,6 +4,7 @@ from colorama import Fore, init
 from json import load, JSONDecodeError
 from argparse import ArgumentParser
 from sys import exit
+from charset_normalizer import from_path
 
 # colorama setup
 init()
@@ -33,42 +34,43 @@ def parse_arguments():
     return parser.parse_args()
 
 def main():
-	args = parse_arguments()
-	new_dir = f"{args.directory}/cleaned_files"
-	filter_list = load_json(args.filter)
-	text_files = [f"{args.directory}/{item}" for item in os.listdir(args.directory) if os.path.isfile(f"{args.directory}/{item}")]
-	os.makedirs(f"{new_dir}", exist_ok=True)
+    args = parse_arguments()
+    new_dir = f"{args.directory}/cleaned_files"
+    filter_list = load_json(args.filter)
+    text_files = [f"{args.directory}/{item}" for item in os.listdir(args.directory) if os.path.isfile(f"{args.directory}/{item}")]
+    os.makedirs(f"{new_dir}", exist_ok=True)
+    for text_file in text_files:
 
-	for text_file in text_files:
+        # keep count of regex matches
+        count = 0
 
-		# keep count of regex matches
-		count = 0
+        # prepare filename
+        filename = f"{cyan}{os.path.basename(text_file)}{reset}"
+        
+        # load the file using charset_normalizer's from_path()
+        try:
+            cnf = from_path(text_file).best()
+            text = str(cnf)
+            enc_type = cnf.encoding
+        except IOError:
+            print(f"{red}[X]{reset} FAILED TO READ {filename}")
+            continue
 
-		# prepare filename
-		filename = f"{cyan}{os.path.basename(text_file)}{reset}"
-
-		try:
-			with open(text_file, "r") as f:
-				text = f.read()
-		except UnicodeDecodeError:
-			print(f"{red}[X]{reset} FAILED TO READ {filename}")
-			continue
-
-		# loop through the filter_list and remove the regex matches with the substitute word
-		# increment the count variable by the number of matches for each regex
-		for regex, substitute in filter_list.items():
-			count += len(re.findall(regex, text))
-			text = re.sub(regex, substitute, text, flags=re.IGNORECASE)
+        # loop through the filter_list and remove the regex matches with the substitute word
+        # increment the count variable by the number of matches for each regex
+        for regex, substitute in filter_list.items():
+            count += len(re.findall(regex, text))
+            text = re.sub(regex, substitute, text, flags=re.IGNORECASE)
 
 		# write the new text to a new file under the new_dir directory
-		with open(f"{new_dir}/{os.path.basename(text_file)}", "w") as f:
-			f.write(text)
+        with open(f"{new_dir}/{os.path.basename(text_file)}", "w", encoding=enc_type) as f:
+            f.write(text)
 
-		# print the result to the user
-		if count > 0:
-			print(f"{green}[+]{reset} CHANGED {cyan}{count}{reset} MATCHES FROM {filename}")
-		else:
-			print(f"{yellow}[!]{reset} NO CHANGES FROM {filename}")
+        # print the result to the user
+        if count > 0:
+            print(f"{green}[+]{reset} CHANGED {cyan}{count}{reset} MATCHES FROM {filename}")
+        else:
+            print(f"{yellow}[!]{reset} NO CHANGES FROM {filename}")
 
 if __name__ == "__main__":
 	main()
