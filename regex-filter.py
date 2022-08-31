68e5213db173a7d36c675ed214b2d40aced0f061
illbison <105686563+illbison@users.noreply.github.com>
2022-08-31 23:34:19 +0300

    Code cleanup

--- regex-filter.py
+++ regex-filter.py
@@ -28,9 +28,8 @@ def load_json(json_file):
 
 
 def clean_files(filter_list, new_dir):
-    for text_file in [f"{new_dir}/{item}" for item in os.listdir(new_dir)]:
+    for text_file in [os.path.join(new_dir, item) for item in os.listdir(new_dir)]:
         count = 0
-
         try:
             enc_type = from_path(text_file).best().encoding
             f = open(text_file, "r+", encoding=enc_type)
@@ -63,9 +62,9 @@ def rename_files(filter_list, new_dir):
             new_name = sub(regex, substitute, new_name)
 
         if new_name != text_file:
-            if os.path.exists(f"{new_dir}/{new_name}"):
+            if os.path.exists(os.path.join(new_dir, new_name)):
                 new_name = "".join([choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890") for _ in range(5)]) + "_" + new_name
-            os.rename(f"{new_dir}/{text_file}", f"{new_dir}/{new_name}")
+            os.rename(os.path.join(new_dir, text_file), os.path.join(new_dir, new_name))
             print(f"{Fore.GREEN}[+]{Fore.RESET} RENAMED {Fore.CYAN}{text_file}{Fore.RESET} TO {Fore.CYAN}{new_name}{Fore.RESET}")
         else:
             print(f"{Fore.YELLOW}[!]{Fore.RESET} DID NOT RENAME {Fore.CYAN}{text_file}{Fore.RESET}")
@@ -101,9 +100,9 @@ def main():
         print(f"{Fore.RED}[X]{Fore.RESET} {Fore.CYAN}{args.directory}{Fore.RESET} IS NOT A DIRECTORY")
         exit(1)
 
-    new_dir = f"{args.directory}/cleaned_files"
+    new_dir = os.path.join(args.directory, "cleaned_files")
     filter_list = load_json(args.filter)
-    text_files = [f"{args.directory}/{item}" for item in os.listdir(args.directory) if os.path.isfile(f"{args.directory}/{item}")]
+    text_files = [os.path.join(args.directory, item) for item in os.listdir(args.directory) if os.path.isfile(os.path.join(args.directory, item))]
 
     if not text_files:
         print(f"{Fore.RED}[X]{Fore.RESET} NO FILES IN DIRECTORY")
