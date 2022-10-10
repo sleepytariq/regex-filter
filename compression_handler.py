f539f222c2daab56d36d2f4abeaaa7ab9f46f8d0
illbison <105686563+illbison@users.noreply.github.com>
2022-10-10 15:38:30 +0300

    Add support for bzip2 and lzma compression formats

--- compression_handler.py
+++ compression_handler.py
@@ -4,11 +4,23 @@ import zipfile
 import tarfile
 import py7zr
 import gzip
+import bz2
+import lzma
 
 
 def is_gzipfile(path: str):
     with open(path, "rb") as f:
         return f.read(2) == b"\x1f\x8b"
+
+
+def is_bzip2file(path: str):
+    with open(path, "rb") as f:
+        return f.read(3) == b"\x42\x5a\x68"
+
+
+def is_lzmafile(path: str):
+    with open(path, "rb") as f:
+        return f.read(5) == b"\xfd\x37\x7a\x58\x5a"
 
 
 def is_compressed(path: str):
@@ -20,6 +32,8 @@ def is_compressed(path: str):
         or tarfile.is_tarfile(path)
         or py7zr.is_7zfile(path)
         or is_gzipfile(path)
+        or is_bzip2file(path)
+        or is_lzmafile(path)
     )
 
 
@@ -31,6 +45,7 @@ def decompress(path: str):
             with zipfile.ZipFile(path, "r") as zf:
                 zf.extractall(temp)
             arctype = "zip"
+
         elif tarfile.is_tarfile(path):
             if path.endswith("gz"):
                 tartype = ":gz"
@@ -38,22 +53,35 @@ def decompress(path: str):
                 tartype = ":bz2"
             elif path.endswith("xz"):
                 tartype = ":xz"
-            elif path.endswith("lzma"):
-                tartype = ":lzma"
             else:
                 tartype = ""
             with tarfile.open(path, "r" + tartype) as tf:
                 tf.extractall(temp)
             arctype = "tar" + tartype
+
         elif py7zr.is_7zfile(path):
             with py7zr.SevenZipFile(path, "r") as zf:
                 zf.extractall(temp)
             arctype = "7zip"
+
         elif is_gzipfile(path):
             with gzip.open(path, "rb") as gzf:
                 with open(temp, "wb") as f:
                     f.write(gzf.read())
             arctype = "gzip"
+
+        elif is_bzip2file(path):
+            with bz2.open(path, "rb") as bzf:
+                with open(temp, "wb") as f:
+                    f.write(bzf.read())
+            arctype = "bzip2"
+
+        elif is_lzmafile(path):
+            with lzma.open(path, "rb") as lf:
+                with open(temp, "wb") as f:
+                    f.write(lf.read())
+            arctype = "lzma"
+
         else:
             return
         os.remove(path)
@@ -97,6 +125,16 @@ def compress(path: str, arctype: str):
         with open(path, "rb") as f:
             with gzip.open(temp, "wb") as gzf:
                 gzf.write(f.read())
+
+    elif arctype == "bzip2":
+        with open(path, "rb") as f:
+            with bz2.open(temp, "wb") as bzf:
+                bzf.write(f.read())
+
+    elif arctype == "lzma":
+        with open(path, "rb") as f:
+            with lzma.open(temp, "wb") as lf:
+                lf.write(f.read())
 
     else:
         return
